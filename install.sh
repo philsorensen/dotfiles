bd5213a5583b160e43fec934afff56e1cb2c8610
Phillip Sorensen <pas37@cornell.edu>
2024-03-18 17:11:08 -0400

    Move ansible config and collections to .ansible

--- install.sh
+++ install.sh
@@ -8,19 +8,23 @@
 #  - any other parameter  -> run run.yaml playbook with paramter as
 #                           module
 #
+cd "$(dirname ${BASH_SOURCE[0]})/.ansible"
+
+
 tags="untagged"
 
 # check for ansible-playbook
 if ! command -v ansible-playbook >/dev/null; then
     echo "Need ansible-playbook on the PATH"
     exit 1
- fi
+fi
 
 # set tags
 [ "$(hostname | sed 's/[^\.]*\.//')" == "classe.cornell.edu" ] \
                                               && tags="${tags},classe"
 [ "$(stat --format=%T -f ${HOME})" == "nfs" ] && tags="${tags},shared-home"
 
+# run playbooks
 if [ -z "$1" ]; then
     # run full install/update
     #  - install collections/roles
@@ -29,12 +33,12 @@ if [ -z "$1" ]; then
     #  - run dotfiles.yaml playbook
     ansible-galaxy install -r requirements.yaml
 
-    if ansible-playbook shell.yaml --tags="${tags}"; then
+    if ansible-playbook ../shell.yaml --tags="${tags}"; then
         source ${HOME}/.bash_profile
-        ansible-playbook dotfiles.yaml --tags="${tags}"
+        ansible-playbook ../dotfiles.yaml --tags="${tags}"
     fi
 elif [ "$1" == "shell" ]; then
-    ansible-playbook shell.yaml --tags="${tags}"
+    ansible-playbook ../shell.yaml --tags="${tags}"
 else
-    ansible-playbook run.yaml --tags="${tags}" --extra-vars="module=$1"
+    ansible-playbook ../run.yaml --tags="${tags}" --extra-vars="module=$1"
 fi
