107c5f3380c37f00402b05b047cc35888fc687a9
Phillip Sorensen <pas37@cornell.edu>
2024-04-22 12:38:21 -0400

    Update vivaldi wrapper to use user-directory instead of profiles

--- wrappers/vivaldi-wrapper.sh
+++ wrappers/vivaldi-wrapper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #  Wrapper script for hostname based vivaldi profiles
 vivaldi_bin=/opt/vivaldi/vivaldi
+vivaldi_user_dir=${USER_DIR}/Profiles/vivaldi
 
-profile=$(hostname -s)
-[ -n "${NX_SESSION_ID}" ] && profile="${profile}-NX"
+user_dir="${vivaldi_user_dir}/$(hostname -s)"
+[ -n "${NX_SESSION_ID}" ] && user_dir="${user_dir}-NX"
 
 exec $vivaldi_bin --disk-cache-size=24000000 \
                   --proxy-pac-url=https://www.classe.cornell.edu/proxy.pac \
-                  --profile-directory=$profile "$@"
+                  --user-data-dir=$user_dir \
+                  --profile-directory=Default "$@"
